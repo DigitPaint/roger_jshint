0b2a2888fa2f97397b83ed695f76b631b451781b
Edwin van der Graaf <edwin@digitpaint.nl>
2015-03-09 14:21:04 +0100

    Add generator for .jshintrc
    Fixes #3

---
+++ lib/roger_jshint/generator.rb
@@ -0,0 +1,45 @@
+require "open-uri"
+
+require "roger/cli/generate"
+require "roger/cli/command"
+require "roger/generators"
+
+module RogerJsHint
+  # Lint configuration retriever
+  class Generator < Roger::Generators::Base
+    include Thor::Actions
+    CONFIG_PATH = ".jshintrc"
+    DEFAULT_CONFIG_URL = "https://raw.githubusercontent.com/"\
+                         "DigitPaint/javascript/master/linters/jshintrc"
+
+    desc "JsHint generator creates an .jshintrc config file"
+    class_option(
+      :config,
+      type: :string,
+      aliases: ["-c"],
+      desc: "Optional config file to use takes a path or url, by
+             default it uses the company' default"
+    )
+
+    class_option(
+      :force,
+      type: :boolean,
+      aliases: ["-f"],
+      desc: "Always overwrite the config file"
+    )
+
+    # Write config file
+    def write_config_file
+      if options[:config]
+        config = open(options[:config]).read
+      else
+        config = open(DEFAULT_CONFIG_URL).read
+      end
+
+      # Create file check if we don't have a conflict or something
+      create_file "#{@project.path}/#{CONFIG_PATH}", config, force: options[:force]
+    end
+  end
+end
+
+Roger::Generators.register :jshint, RogerJsHint::Generator
